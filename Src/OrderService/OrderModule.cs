b28ecebc46280b724fda2b4d4f3ad2eb6e3bc5fe
ChristopherWolf <christopherw@iqmetrix.com>
2015-01-14 12:10:55 -0600

    added order module

---
+++ Src/OrderService/OrderModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Nancy;
+
+namespace OrderService
+{
+	public enum OrderState
+	{
+		New,
+		InProgress,
+		Shipped,
+		Error
+	}
+
+	public class Order
+	{
+		public Guid Id { get; set; }
+
+		public string CustomerName { get; set; }
+
+		public string CustomerAddress { get; set; }
+
+		public IEnumerable<int> ItemIds { get; set; }
+
+		public OrderState State { get; set; }
+	}
+
+	public class OrderModule : NancyModule
+	{
+		public OrderModule()
+		{
+			Get["/orders/{id}"] = x => new Order() { Id = x.id, CustomerName = "Bob Smith"};
+		}
+	}
+}
